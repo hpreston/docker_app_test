b22ea445a4ac80f92011de2163fb9e0b76611853
Hank Preston <hank.preston@gmail.com>
2016-03-05 19:51:02 -0500

    Some cleanup

---
+++ README.md
@@ -0,0 +1,22 @@
+# docker_app_test
+
+Simple Test Application for learning how to build and package docker apps.
+
+
+## Environment
+
+Required
+* argparse
+* flask
+
+# Installation
+
+    pip install -r requirements.txt
+
+# Usage
+
+    python docker_app_test.py -p <portnumber>
+
+# Accessing
+
+    http://localhost:portnumber
--- docker_app_test/docker_app_test.py
+++ docker_app_test/docker_app_test.py
@@ -1,12 +1,24 @@
 from flask import Flask
+from argparse import ArgumentParser
+import socket
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    sysname = socket.gethostname()
+
+    return 'Hello World! from ' + sysname
+
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    parser = ArgumentParser('Sample Application')
+    parser.add_argument(
+        '-p', '--port', help='Port to listen on ', required=False, default=5000)
+    args = parser.parse_args()
+    app.secret_key = '1234'
+    port = int(args.port)
+    app.run(host='0.0.0.0', port=port, debug=True)
